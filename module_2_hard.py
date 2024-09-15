e6a8ff6e3de6b2f97580bcde9b49dfc8b9fb4c4a
Anna-277 <yuava@inbox.ru>
2024-09-15 23:33:39 +0500

    Дополнительное практическое задание по модулю: "Основные операторы" Задание "Слишком древний шифр"

---
+++ module_2_hard.py
@@ -0,0 +1,21 @@
+def password(n):
+    result = ''
+    for i in range(1, n):
+        for j in range(i + 1, n):
+            if n % (i + j) == 0:
+                result += str(i) + str(j)
+    return result
+
+
+print(password(int(input('Введите число: '))))
+print(password(int(input('Введите число: '))))
+print(password(int(input('Введите число: '))))
+print(password(int(input('Введите число: '))))
+print(password(int(input('Введите число: '))))
+print(password(int(input('Введите число: '))))
+print(password(int(input('Введите число: '))))
+print(password(int(input('Введите число: '))))
+print(password(int(input('Введите число: '))))
+print(password(int(input('Введите число: '))))
+print(password(int(input('Введите число: '))))
+print(password(int(input('Введите число: '))))
